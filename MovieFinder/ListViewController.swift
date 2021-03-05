f11ad1cdb33051686940c26af6bb0dbc35976e8f
Panchami <panchami.rao.i@globaldelight.com>
2021-03-05 10:05:24 +0530

    Implement search API

--- MovieFinder/ListViewController.swift
+++ MovieFinder/ListViewController.swift
@@ -11,6 +11,8 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
     
     var globalURL:String="https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"
     
+    var searchURL:String="https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/"
+    
     let headers = [
         "x-rapidapi-key": "0aa8f88433msh99f545de959cc9bp16bc3ajsn4dd85324e7f6",
         "x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com"
@@ -22,11 +24,13 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
     var movieImage:String=""
     var plotDescription:String=""
     var jsonArray=[[String:Any]]()
+    var searchArray=[[String:String]]()
     
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-    
+
+    @IBOutlet weak var imageIcon: UIImageView!
     
 
     override func viewDidLoad() {
@@ -41,7 +45,7 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
      }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return searchArray.count
     }
     
    
@@ -52,27 +56,42 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
         
         cell.textLabel?.numberOfLines=0
         
-        cell.textLabel?.text="Title: \(titleOfMovie)"+"\n" + "Rating: \(rating)" + "\n" + "Year of release: \(year)"
+        //cell.textLabel?.text="Title: \(titleOfMovie)"+"\n" + "Rating: \(rating)" + "\n" + "Year of release: \(year)"
+        
+        cell.textLabel?.text=searchArray[indexPath.row]["title"]
       
         return cell
     }
     
     
-   
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        globalURL+=searchBar.text!
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+       // globalURL+=(tableView.cellForRow(at: indexPath)?.textLabel?.text)!
+        
+        globalURL+=searchArray[indexPath.row]["id"]!
+        
         print(globalURL)
         
         getMovieData(globalURL)
         
-        //self.tableView.reloadData()
     }
+    
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+         searchURL+=searchBar.text!
+        
+        getSearchData(searchURL)
+        
+    }
+    
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         searchBar.text=""
+        
+        searchURL="https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/"
         globalURL="https://imdb-internet-movie-database-unofficial.p.rapidapi.com/film/"
-        print(globalURL)
+        //print(globalURL)
     }
     
     
@@ -106,6 +125,45 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
                         self.movieImage=json["poster"] as! String
                         self.plotDescription=json["plot"] as! String
                         self.jsonArray = (json["cast"] as? [[String: Any]])!
+                        
+                        DispatchQueue.main.async{
+                            self.performSegue(withIdentifier: "moveToDetailPage", sender: self)
+                        }
+                    }
+                } catch let error as NSError {
+                    print("Failed to load: \(error.localizedDescription)")
+                }
+                
+            }
+           
+        })
+
+        
+        dataTask.resume()
+    
+    }
+    
+    
+    func getSearchData(_ URLL:String) {
+        
+        let request = NSMutableURLRequest(url: NSURL(string: URLL)! as URL,
+                                                cachePolicy: .useProtocolCachePolicy,
+                                            timeoutInterval: 10.0)
+        request.httpMethod = "GET"
+        request.allHTTPHeaderFields = headers
+
+        let session = URLSession.shared
+        let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+            if (error != nil) {
+                print(error)
+            }  else {
+                //let httpResponse = response as? HTTPURLResponse
+                
+                do {
+                    // make sure this JSON is in the format we expect
+                    if let json = try JSONSerialization.jsonObject(with: data!, options: []) as? [String: Any] {
+                        // try to read out a string array
+                        self.searchArray = (json["titles"] as? [[String: String]])!
                     }
                 } catch let error as NSError {
                     print("Failed to load: \(error.localizedDescription)")
@@ -118,13 +176,12 @@ class ListViewController: UIViewController,UITableViewDataSource, UITableViewDel
            
         })
 
-        
         dataTask.resume()
-    
+        
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier=="moveToDetailedView" {
+        if segue.identifier=="moveToDetailPage" {
             let destinationVC=segue.destination as! DetailViewController
             
             destinationVC.poster=movieImage
